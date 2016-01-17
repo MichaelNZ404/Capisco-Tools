4dd1c75b4571d47479cc9a9a330f578484668dac
MichaelNZ404 <michael.coleman404@gmail.com>
2016-01-18 00:45:03 +1300

    Removed two unused imports

--- src/capisco/lucene_extractor/LuceneExtractor.java
+++ src/capisco/lucene_extractor/LuceneExtractor.java
@@ -4,8 +4,6 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.HashSet;
 
 import org.apache.lucene.document.Document;
