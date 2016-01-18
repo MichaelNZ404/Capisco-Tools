d3b0834af7bb40ff1fadd4e5d2ec096fe8b8e828
mjc62 <mjc62@tanekaha.cms.waikato.ac.nz>
2016-01-19 12:56:20 +1300

    Added in support for volume and page based extraction

--- src/capisco/lucene_extractor/LuceneExtractor.java
+++ src/capisco/lucene_extractor/LuceneExtractor.java
@@ -19,57 +19,105 @@ import org.apache.lucene.store.FSDirectory;
 
 public class LuceneExtractor
 {
-	final static String index = "/home/mjc62/Benchmark2/indexes/capisco/";
-	final static String output = "/home/mjc62/Benchmark2/output/capisco/";
-	final static String namefield = "path"; //path for Capisco
-	final static String valuefield = "topics"; //value for Capisco
+	final static String namefield = "path";
+	final static String valuefield = "topics";
+	final static String pagefield = "pageNum";
+	final static String countfield = "count";
 	
-//	final static String index = "/home/mjc62/Benchmark2/indexes/solr/";
-//	final static String output = "/home/mjc62/Benchmark2/output/solr/";
-//	final static String namefield = "path"; //resourcename for Solr
-//	final static String valuefield = "topics"; //value for Solr
+	private static String index;
+	private static String output;
+	private static String source;
+	private static luceneInfo information;
 	
+	private static class luceneInfo{
+		public String name;
+		public String[] concepts;
+		public String[] counts;
+		public String[] pages;
+		
+		luceneInfo(String name, String[] concepts, String[] counts, String[] pages){
+			this.name = name;
+			this.concepts = concepts;
+			this.counts = counts;
+			this.pages = pages;
+		}
+	}
+	
+	/**
+	 * 
+	 * @param args[0] directory of index
+	 * @param args[1] directory of output
+	 * 
+	 * @param args[2] directory of source files -- optional for bookworm
+	 * 
+	 * @throws Exception
+	 */
 	public static void main(String args[]) throws Exception 
 	{
-		IndexReader reader = DirectoryReader.open(FSDirectory.open(new File(index).toPath()));
-		IndexSearcher searcher = new IndexSearcher(reader);
-		searching(searcher);
+		index = args[0];
+		output = args[1];
+		
+		build();
+		textDump();
+		bookwormDump();
+		if(args.length > 2){
+			source = args[2];
+			greenstoneDump();
+		}
 	}   
     
-	public static void searching(IndexSearcher searcher) throws ParseException, IOException
-	{	
-		HashSet<String> docNames = new HashSet<String>();
+	private static void build() throws IOException{
 		IndexReader reader = DirectoryReader.open(FSDirectory.open(new File(index).toPath()));
+		IndexSearcher searcher = new IndexSearcher(reader);
+		
+		//get all page/volume names in index
+		HashSet<String> docNames = new HashSet<String>();
 		for (int i=0; i<reader.maxDoc(); i++) {
 		    Document doc = reader.document(i);
-		    docNames.add(doc.get(namefield)); //TODO: make sure is unique - hashmap
+		    docNames.add(doc.get(namefield));
 		}
 		
+		//for each name, get the associated term
 		for(String docName : docNames){	
+			String docTitle = docName.substring(docName.lastIndexOf("/")+1) + ".txt";
+			
 			Term term = new Term(namefield, docName);
 			TermQuery tq = new TermQuery(term);		
 			TopDocs results = searcher.search(tq, 1000);
 			ScoreDoc[] hits = results.scoreDocs;
-			String docTitle = docName.substring(docName.lastIndexOf("/")+1);
-			System.out.println(docTitle);
 			
-			File docDir = new File(output);
-			if (!docDir.exists()){
-				docDir.mkdir();
-			}
-			
-			File writename = new File(output + docTitle); 
-        	
-        	BufferedWriter out = new BufferedWriter(new FileWriter(writename,true));
-			for (int i = 0; i < hits.length; ++i) 
-			{
-				Document doc = searcher.doc(hits[i].doc);
-				String[] resultConcepts = doc.getValues(valuefield);				 	
-				for(String concept : resultConcepts){					 
-            	out.write(concept + "\r\n");
-				}							
-			}
-			out.close();
+        	Document doc = searcher.doc(hits[0].doc);
+			information = new luceneInfo(docTitle, doc.getValues(valuefield), doc.getValues(countfield), doc.getValues(pagefield));
 		}
+	}
+	
+	private static void textDump() throws IOException
+	{	
+		File docDir = new File(output);
+		if (!docDir.exists()){
+			docDir.mkdir();
+		}
+	
+		File writename = new File(output + information.name);
+		BufferedWriter out = new BufferedWriter(new FileWriter(writename,true));
+		if(information.pages.length > 0){
+			for(int j = 0; j < information.concepts.length; j++){					 
+        	out.write(information.concepts[j] + "\t" + information.counts[j] + "\t" + information.pages[j] + "\n");
+			}
+		}
+		else{
+			for(int j = 0; j < information.concepts.length; j++){					 
+            	//out.write(resultConcepts[j] + "\t" + resultCounts[j] + "\n");
+            	out.write(information.concepts[j] + "\n");
+			}
+		}
+		out.close();
+	}
+	
+	public static void greenstoneDump(){
+		
+	}
+	public static void bookwormDump(){
+		
 	}
 }
